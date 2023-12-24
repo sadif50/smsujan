a86e73a29916b9a4fb6d9f86a48062defbb8f85e
S. M. Sujan <smsujon50@gmail.com>
2023-12-24 22:39:24 +0600

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.center}>Welcome SM</h1>
+    </main>
+  );
+}
